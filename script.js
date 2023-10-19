73772aebddfaddac0cb24e3814e0051dfa5a4fb5
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-10-19 23:38:53 +0300

    The arithmetic square root of a number

---
+++ script.js
@@ -0,0 +1,36 @@
+// Function to calculate the arithmetic square root of a number.
+function calculateSqrt(number) {
+	const getNumber = parseFloat(number);
+
+	if (isNaN(getNumber) || !isFinite(getNumber)) {
+		return 'Error: Please, enter a number.';
+	}
+
+	if (getNumber < 0) {
+		return 'Please, enter a positive number.';
+	}
+
+	const squareRoot = Math.sqrt(getNumber);
+	const formattedResult = squareRoot % 1 === 0 ? squareRoot : squareRoot.toFixed(3);
+
+	return `The arithmetic square root of ${getNumber} is ${formattedResult}.`;
+}
+
+// Function to get a number from the user and calculate its square root.
+function getSqrt() {
+	const numberInput = prompt('Enter a number:');
+
+	if (numberInput === null || numberInput.trim() === '') {
+		console.log('Error: Please, enter a number!');
+	} else {
+		if (!/^[+-]?\d*\.?\d+$/.test(numberInput)) {
+			console.log('Error: Please, enter a correct positive number.');
+		} else {
+			const result = calculateSqrt(numberInput);
+			console.log(result);
+		}
+	}
+}
+
+// Call the getSqrt function to calculate the square root.
+getSqrt();
